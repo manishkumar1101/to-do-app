8bdcd9b398a1f095a2f737800f168e7c0b3e0288
manishkumar-11 <manishkumar.110104@gmail.com>
2023-10-29 20:13:28 +0530

    configure the intial setup for the application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >     
+      <div className='App'>
+      <input type='text'></input>      
+      <button>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
